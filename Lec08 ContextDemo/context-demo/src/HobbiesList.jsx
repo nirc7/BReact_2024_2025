227109147e9b3b323b66e6f57f5c2971135cd345
nc <31476486+nirc7@users.noreply.github.com>
2024-12-23 15:28:08 +0200

    Update HobbiesList.jsx

--- Lec08 ContextDemo/context-demo/src/HobbiesList.jsx
+++ Lec08 ContextDemo/context-demo/src/HobbiesList.jsx
@@ -7,9 +7,7 @@ export default function HobbiesList(props) {
     const { hobbies, RemoveHobby } = useContext(HobbyContext);
 
     const hobbiesStr = hobbies.map(hob =>
-        <div key={hob.id}>id={hob.id} name={hob.name}
-            times={hob.times}
-            <span
+        <div key={hob.id}>id={hob.id} name={hob.name}             times={hob.times}            <span
                 style={{ color: 'red', margin: 10, cursor: 'pointer' }}
                 onClick={() => RemoveHobby(hob.id)}
             >X</span> </div>
